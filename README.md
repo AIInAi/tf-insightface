754a99b27026c764eae10685e473b66d287d52cd
KleinYuan <kaiwen.yuan1992@gmail.com>
2018-06-09 22:57:27 -0700

    Polish it to make it more friendly

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 - [ ] InsightFace Inference Demo
 
 - [ ] InsightFace training pipeline
+
+# How to run it
+
+* Install Depenencies: `pip install -r requirements.txt`
+
+* Download pre-trained [frozen model](https://drive.google.com/open?id=1Iw2Ckz_BnHZUi78USlaFreZXylJj7hnP) and put it under [`pretrained` folder](https://github.com/AIInAi/tf-insightface/tree/master/pretrained)
+
+* Run demo: `python apps/example.py`
+
+* You shall be able to see terminal output a 512 element array representing face feature embedded
--- apps/example.py
+++ apps/example.py
@@ -3,13 +3,21 @@ import numpy as np
 from models import base_server
 from configs import configs
 
+# Read example image
 test_img = cv2.imread(configs.test_img_fp)
 test_img = cv2.resize(test_img, (112, 112))
+
+# Define input tensors feed to session graph
 dropout_rate = 0.5
 input_data = [np.expand_dims(test_img, axis=0), dropout_rate]
 
+# Define a Base Server
 srv = base_server.BaseServer(model_fp=configs.model_fp,
                              input_tensor_names=configs.input_tensor_names,
                              output_tensor_names=configs.output_tensor_names,
                              device=configs.device)
+# Run prediction
 prediction = srv.inference(data=input_data)
+
+# Print results
+print('512-D Features are \n{}'.format(prediction))
